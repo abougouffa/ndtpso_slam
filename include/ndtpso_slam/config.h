33516d300307c0995e9de6d1f9e674a97f444052
Abdelhak Bougouffa <abougouffa@cryptolab.net>
2018-02-25 23:15:21 +0100

    Change the algorithm parameters

--- include/ndtpso_slam/config.h
+++ include/ndtpso_slam/config.h
@@ -1,9 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define LASER_IGNORE_EPSILON 0.05 // Ignore points around the origin with 5cm
-#define PSO_ITERATIONS 25
-#define PSO_POPULATION_SIZE 25
-#define NDT_WINDOW_SIZE 5
+#define LASER_IGNORE_EPSILON 0.2 // Ignore points around the origin with 20cm
+#define PSO_ITERATIONS 50
+#define PSO_POPULATION_SIZE 50
+#define NDT_WINDOW_SIZE 15
 
 #endif // CONFIG_H
