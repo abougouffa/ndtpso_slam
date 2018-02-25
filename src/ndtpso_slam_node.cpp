4771254179ff76a58ae18af68fb26c225e5f428c
Abdelhak Bougouffa <abougouffa@cryptolab.net>
2018-02-25 23:26:05 +0100

    Small node edits

--- src/ndtpso_slam_node.cpp
+++ src/ndtpso_slam_node.cpp
@@ -5,10 +5,10 @@
 #include "sensor_msgs/LaserScan.h"
 #include "std_msgs/Float32.h"
 #include <chrono>
+#include <cstdio>
 #include <eigen3/Eigen/Core>
 #include <iostream>
 #include <mutex>
-#include <stdio.h>
 
 using namespace Eigen;
 using std::cout;
@@ -19,7 +19,7 @@ using std::endl;
 
 static std::mutex matcher_mutex;
 static NDTFrame ref_frame(Vector3d::Zero(), 200, 200, CELL_SIZE);
-static NDTFrame current_frame(Vector3d::Zero(), 50, 50, CELL_SIZE);
+static NDTFrame current_frame(Vector3d::Zero(), 50, 50, 50);
 static NDTFrame global_map(Vector3d::Zero(), 200, 200, CELL_SIZE);
 static Vector3d global_trans, previous_trans, trans_estimate;
 static bool first_iter;
@@ -71,7 +71,7 @@ void scan_mathcher(const sensor_msgs::LaserScan::ConstPtr& scan)
 
     global_map.addPose(current_trans);
 
-    iter_num = (iter_num + 1) % 1;
+    iter_num = (iter_num + 1) % 15;
 
     auto finish = std::chrono::high_resolution_clock::now();
 
@@ -80,7 +80,7 @@ void scan_mathcher(const sensor_msgs::LaserScan::ConstPtr& scan)
     std::cout << endl
               << "Elapsed time: " << elapsed.count() << " s\n";
 
-    current_frame = NDTFrame(Vector3d::Zero(), 50, 50, CELL_SIZE);
+    current_frame = NDTFrame(Vector3d::Zero(), 50, 50, 50);
 #ifdef CELL_SIZE_SMALL
     current_frame_small = NDTFrame(Vector3d::Zero(), 20, 20, CELL_SIZE_SMALL);
 #endif
@@ -95,11 +95,19 @@ int main(int argc, char** argv)
     previous_trans = Vector3d::Zero();
     trans_estimate = Vector3d::Zero();
 
+    printf("PSO Number of Iterations: %d\n"
+           "PSO Population Size: %d\n"
+           "NDT Cell Size: %f\n"
+           "NDT Window Size: %d\n",
+        PSO_ITERATIONS, PSO_POPULATION_SIZE, CELL_SIZE, NDT_WINDOW_SIZE);
+
     ros::init(argc, argv, "ndtpso_slam");
     ros::NodeHandle nh;
     //    ros::Publisher pose_pub = nh.advertise<geometry_msgs::PoseStamped>("ndtpso/pose", 1);
     ros::Subscriber laser_sub = nh.subscribe<sensor_msgs::LaserScan>("/scan", 1, &scan_mathcher);
     ros::Rate loop_rate(10);
+
+    ROS_INFO("NDT-PSO SLAM launched successfuly");
 
     while (ros::ok()) {
         ros::spinOnce();
@@ -108,5 +116,9 @@ int main(int argc, char** argv)
     }
 
     //    ref_frame.saveImage("globalmap-ALL", 80);
-    global_map.saveImage("globalmap-20IT", 20);
+
+    char filename[100];
+    sprintf(filename, "globalmap-08%X", ros::Time::now().sec);
+    global_map.saveImage(filename, 25);
+    printf("Map saved to file %s\n", filename);
 }
