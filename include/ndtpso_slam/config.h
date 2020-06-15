38033bc6dcc34a94138b5772dd2b62b9237235ee
Abdelhak Bougouffa <abdelhak.bougouffa@universite-paris-saclay.fr>
2020-06-15 11:18:09 +0200

    Params change

--- include/ndtpso_slam/config.h
+++ include/ndtpso_slam/config.h
@@ -3,14 +3,15 @@
 
 // Default values
 #define NDT_WINDOW_SIZE 100
+#define NDT_MAX_POINTS_PER_CELL 100 // Unused now, to be used to limit the points/cell by a circular buffer, to ensure const memory
 #define LASER_IGNORE_EPSILON 0.1f // Ignore points around the origin with 10cm
-#define USING_TRANS true
+#define USING_TRANS false
 #define PREFER_FRONTAL_POINTS false // Disabled
 #define BUILD_OCCUPANCY_GRID true
 
 // PSO parameters
-#define PSO_ITERATIONS 50
-#define PSO_POPULATION_SIZE 40
+#define PSO_ITERATIONS 60
+#define PSO_POPULATION_SIZE 25
 #define PSO_W_DUMPING_COEF 1.
 #define PSO_W .8
 #define PSO_C1 2.
