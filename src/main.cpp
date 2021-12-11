8d28b282ada18c70c3a784ab911c697c1bee703c
Abdelhak Bougouffa <abougouffa@cryptolab.net>
2021-12-11 01:53:18 +0100

    Wait for ROS master
    Signed-off-by: Abdelhak Bougouffa <abougouffa@cryptolab.net>

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,29 @@
-#include "ros/ros.h"
 #include "ndtpso_slam_node.hpp"
+#include <cstdlib>
+#include <stdexcept>
+#include <ros/ros.h>
+#include <thread>
 
-int main(int argc, char **argv)
-{
+using namespace std::chrono_literals;
+
+int main(int argc, char** argv) {
   ros::init(argc, argv, "ndtpso_slam");
+
+  while (!ros::master::check()) {
+    ROS_ERROR("Waiting for ROS master at %s", ros::master::getURI().c_str());
+    std::this_thread::sleep_for(1s);
+  }
+
+  ROS_INFO("Connected to ROS master");
+
   ros::NodeHandle nh("~");
 
-  NDTPSONode node(nh);
+  try {
+    NDTPSONode node(nh);
+  } catch (std::runtime_error& ex) {
+    ROS_ERROR("FATAL ERROR, exception occured '%s'", ex.what());
+    std::exit(EXIT_FAILURE);
+  }
 
-
+  std::exit(EXIT_SUCCESS);
 }
